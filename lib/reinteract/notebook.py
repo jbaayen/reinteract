36bae4dcb88f518ceacd1d3112370d8a41bc0b5d
Owen W. Taylor <otaylor@fishsoup.net>
2007-11-17 11:10:39 -0500

    Fix handling of "from a.b import c" When a.b.c doesn't exist after a.b is loaded, we need to try and load a.b.c as a module to match Python's built-in behavior. http://www.reinteract.org/trac/ticket/7

--- lib/reinteract/notebook.py
+++ lib/reinteract/notebook.py
@@ -33,22 +33,10 @@ class Notebook:
                 del sys.modules[self.__prefix + "." + name]
                 del self.__modules[name]
                 return module
-    
-    def __import_recurse(self, names, fromlist):
-        name = ".".join(names)
-        
-        try:
-            return self.__modules[name], True
-        except KeyError:
-            pass
-        
-        try:
-            return sys.modules[name], False
-        except KeyError:
-            pass
 
-        if len(names) == 1:
-            parent = None
+    def __find_and_load(self, fullname, name, parent=None, local=None):
+        if parent == None:
+            assert local == None
             try:
                 f, pathname, description = imp.find_module(name, self.__path)
                 local = True
@@ -56,30 +44,76 @@ class Notebook:
                 f, pathname, description = imp.find_module(name)
                 local = False
         else:
-            child_names = names[0:-1]
-            parent, local = self.__import_recurse(names[0:-1], fromlist)
-            f, pathname, description = imp.find_module(names[-1], parent.__path__)
+            assert local != None
+            f, pathname, description = imp.find_module(name, parent.__path__)
 
         try:
             if local:
-                module = imp.load_module(self.__prefix + "." + name, f, pathname, description)
+                module = imp.load_module(self.__prefix + "." + fullname, f, pathname, description)
                 self.__modules[name] = module
             else:
                 module = imp.load_module(name, f, pathname, description)
 
             if parent != None:
-                parent.__dict__[names[-1]] = module
+                parent.__dict__[name] = module
         finally:
             if f != None:
                 f.close()
 
         return module, local
         
+    def __import_recurse(self, names):
+        fullname = ".".join(names)
+        
+        try:
+            return self.__modules[fullname], True
+        except KeyError:
+            pass
+        
+        try:
+            return sys.modules[fullname], False
+        except KeyError:
+            pass
+
+        if len(names) == 1:
+            module, local = self.__find_and_load(fullname, names[-1])
+        else:
+            parent, local = self.__import_recurse(names[0:-1])
+            module, _ = self.__find_and_load(fullname, names[-1], parent=parent, local=local)
+
+        return module, local
+
+    def __ensure_from_list_item(self, fullname, fromname, module, local):
+        if fromname == "*": # * inside __all__, ignore
+            return
+        
+        if not isinstance(fromname, basestring):
+            raise TypeError("Item in from list is not a string")
+        
+        try:
+            getattr(module, fromname)
+        except AttributeError:
+            self.__find_and_load(fullname + "." + fromname, fromname, parent=module, local=local)
+        
     def do_import(self, name, globals=None, locals=None, fromlist=None, level=None):
         names = name.split('.')
         
-        module, local =  self.__import_recurse(names, fromlist)
+        module, local =  self.__import_recurse(names)
+        
         if fromlist != None:
+            # In 'from a.b import c', if a.b.c doesn't exist after loading a.b, Python
+            # will try to load a.b.c as a module
+            for fromname in fromlist:
+                if fromname == "*":
+                    try:
+                        all = getattr(module, "__all__")
+                        for allname in all:
+                            self.__ensure_from_list_item(name, allname, module, local)
+                    except AttributeError:
+                        pass
+                else:
+                    self.__ensure_from_list_item(name, fromname, module, local)
+                            
             return module
         elif local:
             return self.__modules[names[0]]
