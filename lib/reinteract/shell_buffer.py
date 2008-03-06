4a6da3177431177d75c3550c5741bd6199e338e4
Jon Kuhn <kuhn@mobiusmicro.com>
2008-03-06 09:50:25 -0800

    Fixed issue where Reinteract was unable to save over an existing worksheet file on Windows.

--- lib/reinteract/shell_buffer.py
+++ lib/reinteract/shell_buffer.py
@@ -1323,7 +1323,9 @@ class ShellBuffer(gtk.TextBuffer, Worksheet):
                 f.write(chunk_text)
             
             f.close()
+            os.rename(filename, filename+'.bak')
             os.rename(tmpname, filename)
+            os.unlink(filename+'.bak')
             success = True
 
             self.__set_filename_and_modified(filename, False)
